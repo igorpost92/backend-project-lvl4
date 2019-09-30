93d7fbb82f717dc85e37e6b5721f9784f29b9a20
Igor Postolenko <igorpost92@yandex.ru>
2019-09-30 08:12:15 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// import path from 'path';
+// import webpack from 'webpack';
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: [
+    path.join(__dirname, 'src', 'index.js'),
+  ],
+  // entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'public', 'assets'),
+    filename: 'main.js',
+    publicPath: '/public/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    //   'window.jQuery': 'jquery',
+    //   Popper: ['popper.js', 'default'],
+    // }),
+  ],
+};
